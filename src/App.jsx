a535d91ec0a45e73e0c917c0ba7f3cd725d9f0b0
rzryhn <rizarayhanw@gmail.com>
2026-04-07 16:36:34 +0700

    create header, navbar and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
