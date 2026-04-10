ff55c587977dcaddcc0ee99d95cb5b1683222021
rzryhn <rizarayhanw@gmail.com>
2026-04-10 14:40:05 +0700

    finalized

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,28 +1,149 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const placeOrderSubmit = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+    let response = await axios.post(url + "/api/order/place", orderData, {
+      headers: { token },
+    });
+    if (response.data.success) {
+      const { session_url } = response.data;
+      window.location.replace(session_url);
+    } else {
+      alert("Error");
+    }
+  };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
+      navigate("/cart");
+    }
+  }, [token]);
+
   return (
-    <form className="place-order">
+    <form onSubmit={placeOrderSubmit} className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
+          <input
+            onChange={onChangeHandler}
+            name="firstName"
+            value={data.firstName}
+            type="text"
+            placeholder="First Name"
+            required
+          />
+          <input
+            onChange={onChangeHandler}
+            name="lastName"
+            value={data.lastName}
+            type="text"
+            placeholder="Last Name"
+            required
+          />
         </div>
-        <input type="email" placeholder="Email address" />
-        <input type="text" placeholder="Street" />
+        <input
+          onChange={onChangeHandler}
+          name="email"
+          value={data.email}
+          type="email"
+          placeholder="Email address"
+          required
+        />
+        <input
+          onChange={onChangeHandler}
+          name="street"
+          value={data.street}
+          type="text"
+          placeholder="Street"
+          required
+        />
         <div className="multi-fields">
-          <input type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
+          <input
+            onChange={onChangeHandler}
+            name="city"
+            value={data.city}
+            type="text"
+            placeholder="City"
+            required
+          />
+          <input
+            onChange={onChangeHandler}
+            name="state"
+            value={data.state}
+            type="text"
+            placeholder="State"
+            required
+          />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder="Zip code" />
-          <input type="text" placeholder="Country" />
+          <input
+            onChange={onChangeHandler}
+            name="zipcode"
+            value={data.zipcode}
+            type="text"
+            placeholder="Zip code"
+            required
+          />
+          <input
+            onChange={onChangeHandler}
+            name="country"
+            value={data.country}
+            type="text"
+            placeholder="Country"
+            required
+          />
         </div>
-        <input type="text" placeholder="Phone" />
+        <input
+          onChange={onChangeHandler}
+          name="phone"
+          value={data.phone}
+          type="text"
+          placeholder="Phone"
+          required
+        />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
@@ -45,7 +166,7 @@ const PlaceOrder = () => {
               </b>
             </div>
           </div>
-          <button>PROCEED TO PAYMENT</button>
+          <button type="submit">PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
